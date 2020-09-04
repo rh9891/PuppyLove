036b8d80dd22949443babe7dc86fe8c0246ea4c3
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-03 23:20:21 -0400

    Coded the Navbar. Have all paths of the Navbar working as expected.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-const Navbar = props => <h1>Navbar</h1>
+const Navbar = props => 
+  <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+    <Link className="navbar-brand" to="/">
+      Puppy Love
+    </Link>
+      <ul className="navbar-nav">
+        <li className={window.location.pathname === "/" || window.location.pathname === "/about" ? "active" : ""}>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+        <li className={window.location.pathname === "/" || window.location.pathname === "/discover" ? "active" : ""}>
+          <Link className="nav-link" to="/discover">
+            Discover
+          </Link>
+        </li>
+        <li className={window.location.pathname === "/" || window.location.pathname === "/search" ? "active" : ""}>
+          <Link className="nav-link" to="/search">
+            Search
+          </Link>
+        </li>
+      </ul>
+  </nav>
 
 export default Navbar;
