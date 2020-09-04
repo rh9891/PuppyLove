7c922550d4e4f67379c7a764439238dc570ba5b9
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-04 00:14:05 -0400

    Created hero component and stylesheet. Added code to hero component and passed props.

---
+++ src/components/Hero/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Hero = (props) => (
+  <div
+    className="hero text-center"
+    style={{ backgroundImage: `url(${props.backgroundImage})` }}
+  >
+    {props.children}
+  </div>
+);
+
+export default Hero;
