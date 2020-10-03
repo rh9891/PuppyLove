14541858abca45d5e395762e27d60ac03f50db5f
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-02 20:13:43 -0400

    Added to the Discover component to build the layout.

--- src/components/pages/Discover.js
+++ src/components/pages/Discover.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
-const Discover = () => (
-  <h1 style={{ fontFamily: "Proxima Nova Soft" }}>Discover</h1>
-);
+class Discover extends Component {
+  state = {
+    image: "",
+    matchCount: 0,
+  };
+  //   <h1 style={{ fontFamily: "Proxima Nova Soft" }}>Discover</h1>
+  render() {
+    return (
+      <Fragment>
+        <h1 className="text-center" style={{ fontFamily: "Proxima Nova Soft" }}>
+          Make New Friends
+        </h1>
+        <h3 className="text-center" style={{ fontFamily: "Proxima Nova Soft" }}>
+          Thumbs up if there are any pups that you'd like to meet.
+        </h3>
+        <h1 className="text-center" style={{ fontFamily: "Proxima Nova Soft" }}>
+          Made friends with {this.state.matchCount} pups so far!
+        </h1>
+      </Fragment>
+    );
+  }
+}
 
 export default Discover;
