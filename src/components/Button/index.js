671d67b0870d8074e0be34128ae46aa31d476bc8
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-02 20:31:10 -0400

    Created and added to Button component to be later added to the Discover component.

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Button(props) {
+  return (
+    <button
+      onClick={props.onClick}
+      className={`card-btn ${props["data-value"]}`}
+      {...props}
+    />
+  );
+}
+
+export default Button;
