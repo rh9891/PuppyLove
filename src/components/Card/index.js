1372e07e173620e9fe4d6bdde53a97e6f0c2b897
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-03 23:13:42 -0400

    Created and added style to the Card component.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,2 +1,26 @@
 import React from "react";
+import Button from "../Button";
 import "./style.css";
+
+function Card(props) {
+  return (
+    <div
+      className="card"
+      style={{ backgroundImage: props.image ? `url(${props.image})` : "none" }}
+    >
+      {!props.image && <i className="fas fa-spinner" aria-hidden="true" />}
+      <Button
+        style={{ opacity: props.image ? 1 : 0 }}
+        onClick={props.handleBtnClick}
+        data-value="pass"
+      ></Button>
+      <Button
+        style={{ opacity: props.image ? 1 : 0 }}
+        onClick={props.handleBtnClick}
+        data-value="pick"
+      ></Button>
+    </div>
+  );
+}
+
+export default Card;
