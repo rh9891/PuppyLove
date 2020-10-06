ecb5102e9e09028cf47fd7f33b743f72288e2cbd
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-05 22:17:39 -0400

    Imported the form functionality into the Search component. Console logged the handleInputChange for the form to make sure that the state was being properly updated. The application is working as expected.

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -10,13 +10,18 @@ class Search extends Component {
     results: [],
   };
 
+  handleInputChange = (event) => {
+    this.setState({ search: event.target.value });
+    console.log(this.state.search);
+  };
+
   render() {
     return (
       <Container style={{ fontFamily: "Proxima Nova Soft", minHeight: "80%" }}>
         <h1 className="text-center" style={{ fontFamily: "Proxima Nova Soft" }}>
           Search By Breed
         </h1>
-        <Form />
+        <Form handleInputChange={this.handleInputChange} />
         <Results />
       </Container>
     );
