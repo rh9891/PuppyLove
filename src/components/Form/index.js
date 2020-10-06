0ea89f5320877560c7f7b77f4824dcd549f028b3
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-05 22:12:18 -0400

    Added functionality for search form.

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./style.css";
 
-const Form = (props) => <form></form>;
+const Form = (props) => (
+  <form className="search">
+    <div className="form-group">
+      <label htmlFor="breed">Breed Name:</label>
+      <input
+        value={props.search}
+        onChange={props.handleInputChange}
+        name="breed"
+        type="text"
+        className="form-control"
+        placeholder="Search a dog breed..."
+        id="breed"
+      />
+    </div>
+  </form>
+);
 
 export default Form;
